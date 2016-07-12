74ccd4b9d8cab42047bd73afadb197de0e807cac
AllergicGorilla <ViniciusC14H18N2O5@gmail.com>
2016-07-12 19:08:51 -0300

    Add tic tac toe

---
+++ tic tac toe/game.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from board import board
+
+B = board()
+player = (input("Choose \'X\' or \'O\':\n") == "X")
+B.isPlayerX(player)
+
+while B.playAgain:
+	B.update()
+	B.draw()
+	print()
+	
+
+print("END")
